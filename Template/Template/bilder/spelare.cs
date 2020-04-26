8a11680a412e68b22ac403e514dcf2fe482ca8ed
alban <alban2001@hotmail.com>
2020-04-26 15:11:44 +0200

    blev klar med koden för spelaren. La till en klass för bollen.

--- Template/Template/bilder/spelare.cs
+++ Template/Template/bilder/spelare.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,30 @@ using System.Threading.Tasks;
 
 namespace Template.bilder
 {
-    public class spelare
+    public class spelare : Bilder
     {
+        public Spelare(Texture2D texture)
+            : base(texture)
+        {
+            hastighet = 5f;
+        }
 
+        public override void Update(GameTime gameTime, List<Bilder> bilders)
+        {
+            if (input == null)
+                throw new Exception("ge ett värde till input");
+
+            if (Keyboard.GetState().IsKeyDown(input.ned))
+                röra.Y = -hastighet;
+            else if (Keyboard.GetState().IsKeyDown(input.Upp))
+                röra.Y = hastighet;
+
+            Position += röra;
+
+            Position.Y = MathHelper.Clamp(Position.Y, 0, Game1.ScreenHeight - bild.Height);
+
+            röra = Vector2.Zero;
+
+        }
     }
 }
